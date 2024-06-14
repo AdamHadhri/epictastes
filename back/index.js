b5d29f43fa5622875f9ed6538234bcf00be65543
AdamHadhri <adamhadhri11@gmail.com>
2024-06-14 20:56:55 +0100

    88

--- back/index.js
+++ back/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Item = require('./back/Models/Item'); // Adjust path as needed
+const Item = require('./Models/Item'); // Adjust path as needed
 const cors = require('cors');
 
 const app = express();
